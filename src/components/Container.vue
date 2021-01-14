fa66050ae2cc3011af5bbc6c04081f4b26046657
Facundo Ignacio Salas <56693254+1w1111876SalasFacundo@users.noreply.github.com>
2021-01-13 21:11:02 -0300

    Proyecto nuevo
    vue todo app

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -7,8 +7,37 @@
     <button @click="Create">+</button>
     <TodoList
       :TodoLists="TodoLists"
-      :Delete="Delete"
+      :DoneLists="DoneLists"
+      :done=false
     />
+   
+  <h3>tareas pendientes</h3>
+  <ul>
+    <li
+      v-for="(todolist, index) in TodoLists"
+      :key="index"
+    >
+    
+      <span class="name">{{todolist.name}}  <input type="checkbox" id="checkbox" @click="checkbox"/></span>
+      
+
+    </li>
+  </ul>
+
+  <h3>Tareas Terminadas</h3>
+  <ul>
+    <li
+      v-for="(donelist, index) in DoneLists"
+      :key="index"
+    >
+    
+      <span class="name">{{donelist.name}}  <input  type="checkbox" id="checkbox" @click='checkbox'/></span>
+      
+
+    </li>
+  </ul>
+
+
   </div>
 </template>
 
@@ -23,20 +52,34 @@ export default {
     let exampleData = [
       { id: 1, name: 'Intentado hacer funcion enviar' },
       { id: 2, name: 'Cenar' },
-      { id: 3, name: 'agregar checkbox' },
+      { id: 3, name: 'agregar checkbox'},
+    ];
+        let exampleDataDone = [
+      { id: 1, name: 'Tarea Hecha'},
     ];
     return {
-      TodoLists: ref(exampleData)
+      TodoLists: ref(exampleData),
+      DoneLists: ref(exampleDataDone),
     };
   },
   methods: {
     Create() {
       let id = 4;
       id += 1;
-      const next = { id, name: this.msg };
+      var done = false;
+      const next = { id, name: this.msg,done};
       this.TodoLists.push(next);
+    },
+        checkbox() {
+       var done = true;
+      if (done === true) {
+       this.DoneLists.push(this.TodoLists[0])
+      } else {
+        this.TodoLists.push(this.DoneLists[0])
+      }
     }
 
+    }
   }
-}
+  
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,53 @@
 <template>
-<h3>tareas pendientes</h3>
-  <ul>
+<h3></h3>
+ <!--  <ul>
     <li
       v-for="(todolist, index) in TodoLists"
       :key="index"
     >
     
-      <span class="name">{{todolist.name}}  <input type="checkbox" id="task-checkbox"/></span>
+      <span class="name">{{todolist.name}}  <input type="checkbox" @click="checkbox"/></span>
       
 
     </li>
   </ul>
+
+  <h3>Tareas Terminadas</h3>
+  <ul>
+    <li
+      v-for="(donelist, index) in DoneLists"
+      :key="index"
+    >
+    
+      <span class="name">{{donelist.name}}  <input  type="checkbox" id="checkbox" @click='checkbox'/></span>
+      
+
+    </li>
+  </ul>
+ -->
+
+
+
 </template>
 
 <script>
 export default {
   props: {
     TodoLists: Array,
+    DoneLists:Array,
+    done:Boolean,
+  },
+  methods : {
+/*     checkbox() {
+       var done = true;
+      if (done == true) {
+        console.log="true";
+        this.TodoLists.push(this.DoneLists[0])
+        this.TodoLists.shift();
+      }
+    } */
   }
+  
 
 }
 </script>
